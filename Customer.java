92c70d780fb9f5f0e393593f8108c67c61e16796
Sukanyareddy <32175072+Sukanyareddy@users.noreply.github.com>
2017-09-26 19:17:21 +0530

    Add files via upload
    customer walks into restaurant

---
+++ Customer.java
@@ -0,0 +1,14 @@
+package rms;
+
+public class Customer {
+    void walk() {
+    	System.out.println("customer walks in to restaurant");
+    }
+
+	public static void main(String[] args) {
+    Customer c=new Customer();	
+    c.walk();
+
+	}
+
+}
