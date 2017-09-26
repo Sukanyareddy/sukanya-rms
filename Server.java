bd4247b001388e57e53439d012d59ed7b4a0ce0b
Sukanyareddy <32175072+Sukanyareddy@users.noreply.github.com>
2017-09-26 22:11:04 +0530

    Add files via upload
    server class for the service to the customer

---
+++ Server.java
@@ -0,0 +1,14 @@
+package rms;
+
+public class Server {
+          void service() {
+          System.out.println("serve to the customer");
+          }
+          
+	public static void main(String[] args) {
+		Server ser=new Server();
+		ser.service();
+
+	}
+
+}
