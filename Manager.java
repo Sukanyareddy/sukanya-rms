c95ccfbec1eab29bea2d5e91bc4895ffd44d35ac
Sukanyareddy <32175072+Sukanyareddy@users.noreply.github.com>
2017-09-27 00:02:32 +0530

    Add files via upload
    manager class for take order from customer

---
+++ Manager.java
@@ -0,0 +1,15 @@
+package rms;
+
+public class Manager {
+	void take_order() {
+		System.out.println("take order from the customer");
+	}
+	
+
+	public static void main(String[] args) {
+		Manager mng=new Manager();
+		mng.take_order();
+
+	}
+
+}
